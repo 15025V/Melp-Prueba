104cd2ae0f67d62f8e4b724066a3d20371d9fca4
15025V <vicogespinoza@gmail.com>
2026-01-16 20:06:30 -0600

    test: probar fetch sin cache en getRestaurants

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@ import { Restaurant } from "@/types/restaurant";
 export async function getRestaurants(): Promise<Restaurant[]> {
   const res = await fetch(
     "https://recruiting-datasets.s3.us-east-2.amazonaws.com/data_melp.json",
-    { cache: "no-store" }
+    // { cache: "no-store" }
   );
 
   if (!res.ok) {
